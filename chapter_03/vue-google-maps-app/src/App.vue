5f3755c922716e3fc553f6f9d4f93a2a906a40b2
Daiki Nagaoka <neko.z.n1210@gmail.com>
2020-02-09 10:04:02 +0900

    add vue app

--- chapter_03/vue-google-maps-app/src/App.vue
+++ chapter_03/vue-google-maps-app/src/App.vue
@@ -1,28 +1,54 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="map">
+    <GmapMap
+      :center="{ lat: 35.7329854, lng: 139.7128128 }"
+      :zoom="15"
+      map-type-id="roadmap"
+      :options="{ disableDefaultUi: true, clickableIcons: false }"
+      style="width: 100%; height: 100%"
+      @click="onMapClick"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="false"
+        :draggable="true"
+        @click="center=m.position"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      markers: []
+    }
+  },
+  methods: {
+    onMapClick(event){
+      this.markers.push({
+        position: {
+          lat: event.latLng.lat(),
+          lng: event.latLng.lng()
+        }
+      })
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#map {
+  height: 100%;
+}
+html,
+body {
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- chapter_03/vue-google-maps-app/src/main.js
+++ chapter_03/vue-google-maps-app/src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import * as VueGoogleMaps from 'vue2-google-maps'
 
 Vue.config.productionTip = false
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: process.env.API_KEY,
+    libraries: 'places,geometry,visualization' // カンマ区切りの文字列で libraries を指定する
+  },
+  installComponents: true
+})
 
 new Vue({
   render: h => h(App),
